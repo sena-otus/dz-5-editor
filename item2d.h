096dfdbd938654a221dcaed84dcfbb2763984147
sena@s73.work <Sergey>
2023-12-21 01:37:22 +0100

    use abstract canvas layer to draw, store color

--- item2d.h
+++ item2d.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "abstractcanvas.h"
 #include "v2d.h"
 
 #include <boost/serialization/access.hpp>
@@ -7,29 +8,25 @@
 class Item2d
 {
 public:
-  Item2d()
-    : m_base{.x = 0, .y = 0}
-  {}
-
+  Item2d(Item2d && other) = default;
   Item2d(const Item2d &other) = default;
-  Item2d(Item2d &&other) = default;
-  Item2d& operator=(const Item2d &other) = default;
-  Item2d& operator=(Item2d &&other) = default;
+  Item2d & operator=(const Item2d &other) = default;
+  Item2d & operator=(Item2d &&other) = default;
   virtual ~Item2d() = default;
 
-  explicit Item2d(v2d && basepoint)
-    : m_base(std::forward<v2d>(basepoint))
-    {}
 
-  v2d base() const { return m_base; }
+  explicit Item2d(int lineColor) : m_lineColor(lineColor) {}
+
+  int lineColor() const {return m_lineColor;}
+
+  virtual void draw(AbstractCanvas &ac) const = 0;
 
   friend class boost::serialization::access;
   template<class Archive>
   void serialize(Archive & ar, const unsigned int version	[[maybe_unused]])
   {
-    ar & m_base;
+    ar & m_lineColor;
   }
-
 private:
-  v2d m_base;
+  int m_lineColor;
 };
