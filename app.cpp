8c93f00fd1a34f03ac21b290a537b9c0b61a7d2d
sena@s73.work <Sergey>
2023-12-21 01:37:43 +0100

    use abstract canvas layer to draw, store color

--- app.cpp
+++ app.cpp
@@ -1,12 +1,14 @@
 #include "app.h"
+#include "tcanvas.h"
 #include "item2d.h"
 
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <fstream>
+#include <memory>
 
 App::App()
-    : m_menu(), m_sketch()
+  : m_menu(), m_sketch()
 {
   m_menu.addItem("Create new sketch", [this] {createNew   ();});
   m_menu.addItem("Import sketch"    , [this] {importSketch();});
@@ -15,6 +17,8 @@ App::App()
   m_menu.addItem("Add circle"       , [this] {addCircle   ();});
   m_menu.addItem("Delete last"      , [this] {delLast     ();});
   m_menu.addItem("Exit"             , [this] {appexit     ();});
+  m_tcanvasptr = std::make_shared<TextCanvas>();
+  m_sketch.setCanvas(m_tcanvasptr);
 }
 
 void App::importSketch()
--- app.h
+++ app.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include "tcanvas.h"
 #include "v2d.h"
 #include "sketch.h"
 #include "menu.h"
 #include <functional>
 
+/** @brief controller class */
 class App
 {
 public:
@@ -22,13 +24,13 @@ public:
   void addLine()
   {
       // TODO: get line coors from user
-    m_sketch.addLine(v2d{.x=1,.y=1}, v2d{.x=0,.y=0});
+    m_sketch.addLine(v2d{.x=1,.y=1}, v2d{.x=0,.y=0}, 1);
   }
 
   void addCircle()
   {
       // TODO: get circle coors from user
-    m_sketch.addCircle(v2d{.x=10,.y=10}, 15);
+    m_sketch.addCircle(v2d{.x=10,.y=10}, 15, 2);
   }
 
   void delLast()
@@ -43,12 +45,16 @@ public:
 
   void run()
   {
-    while(!m_exitFlag) { m_menu.show(); }
+    while(!m_exitFlag) {
+      m_sketch.draw();
+      m_menu.show();
+    }
   }
 
 private:
   Menu m_menu;
   std::string m_fname{"sketch.txt"};
+  std::shared_ptr<TextCanvas> m_tcanvasptr;
   Sketch m_sketch;
   bool m_exitFlag{false};
 };
